08df0e3264048f8ba01f69ada59b0416a469e6ad
BenKung <moon791017@gmail.com>
2018-04-20 17:44:55 +0800

    add qr code scan

---
+++ test.js
@@ -0,0 +1,20 @@
+var QrCode = require('qrcode-reader');
+var fs=require('fs')
+var Jimp = require("jimp");
+var buffer = fs.readFileSync('qrcode.png');
+Jimp.read(buffer, function(err, image) {
+    if (err) {
+        console.error(err);
+        // TODO handle error
+    }
+    var qr = new QrCode();
+    qr.callback = function(err, value) {
+        if (err) {
+            console.error(err);
+            // TODO handle error
+        }
+        console.log(value.result);
+        console.log(value);
+    };
+    qr.decode(image.bitmap);
+});
